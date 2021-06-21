2f0e412c0f1771c2f4c403b492256143e8b53583
valeriomatranga <78118886+valeriomatranga@users.noreply.github.com>
2021-06-21 17:41:47 +0200

    lunghezza paragrafo

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
-$paragrafo = "Prova di testo, inizio lezioni php in data 21/06/2021. Oggi ha Palermo ci sono 36 gradi all'ombra porcaputtana, e io sono morto di caldo. Mi tocca in ogni caso lavorare al computer, ed svolgere l'esercizio lasciato per il pomeriggio!";
-$paragrafo_modificato = str_replace($parola, $censura, $paragrafo);
-//strlen($paragrafo);
 $parola = $_GET['parola'];
 $censura = '***';
+$paragrafo = "Prova di testo, inizio lezioni php in data 21/06/2021. Oggi ha Palermo ci sono 36 gradi all'ombra porcaputtana, e io sono morto di caldo. Mi tocca in ogni caso lavorare al computer, ed svolgere l'esercizio lasciato per il pomeriggio!";
+$paragrafo_modificato = str_replace($parola, $censura, $paragrafo);
 ?>
 
 
@@ -17,11 +16,20 @@ $censura = '***';
 </head>
 <body>
     <h1>Un pomeriggio a Palermo</h1>
+    <span><strong>Testo originale</strong></span>
     <p>
-    <?php
-     echo $paragrafo_modificato;
-     var_dump($_GET);
-
-     ?></p>
+        <?php echo $paragrafo ?>
+    </p>
+    <div>
+        <span>Lunghezza paragrafo: <?php echo strlen($paragrafo); ?></span>
+    </div>
+    <hr>
+    <span><strong>Testo modificato</strong></span>
+    <p>
+        <?php echo $paragrafo_modificato; ?>
+    </p>
+    <div>
+        <span>Lunghezza paragrafo: <?php echo strlen($paragrafo_modificato); ?></span>
+    </div>
 </body>
 </html>
